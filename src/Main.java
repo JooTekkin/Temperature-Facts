0ddc34b91ebc8e3f8a39a61dc4df3bccb2538796
Jootekkin <yusufmustafa1@outlook.com>
2022-10-29 23:21:04 +0200

    Modify README file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         InfoFacts infoFacts = new InfoFacts();
-        infoFacts.testAverageTemperatureWithHighHumidityInFile();
+        infoFacts.testAverageTemperatureInFile();
     }
 }
